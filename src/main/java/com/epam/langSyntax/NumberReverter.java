406a38452824336feb3e43eb239b7eceb159142b
azamatnassir <azamatnsr@gmail.com>
2023-08-03 11:38:36 +0600

    LangSyntax commit

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,16 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int reverse = 0;
+        int remainder;
+
+        while (number != 0) {
+            remainder = number % 10;
+            reverse = (reverse * 10) + remainder;
+            number = number / 10;
+        }
+
+        System.out.println(reverse);
     }
 
 }
